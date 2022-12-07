b6359830d6923040dc3f9db97e73db0aeeab9b79
2022ht66119 <2022ht66119@wilp.bits-pilani.ac.in>
2022-12-07 17:23:30 +0800

    addinv verison

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
     required_version = ">=1.0"
     required_providers {
         azurerm = {
-            source = "hasicorp/azurerm"
+            source = "hashicorp/azurerm"
             version = ">=3.0"
         }
            null = {
-            source = "hasicorp/null"
+            source = "hashicorp/null"
             version = ">=3.0"
         }
     }
