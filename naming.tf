39232ee6571025f8cb0f770ca8c9125d9a28ac16
2022ht66119 <2022ht66119@wilp.bits-pilani.ac.in>
2022-12-07 17:19:03 +0800

    add naming

--- naming.tf
+++ naming.tf
@@ -2,6 +2,6 @@ data "azurerm_subscription" "current" {
 
 }
 locals {
-    name_prefix = data.azurerm_subscription.current.display_name
+    name_prefix = replace(data.azurerm_subscription.current.display_name," ","-")
     name        = format ("az-%s-%s%s", local.name_prefix, var.resource_group_type, var.sequence_number)
 }   
